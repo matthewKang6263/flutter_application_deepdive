16c8b839ec243b58a65439cf338d69fa64b59146
GitHub eso21309 <GitHub eso21309@gmail.com>
2024-11-25 10:11:06 +0900

    UI 완료

--- lib/intro_page.dart
+++ lib/intro_page.dart
@@ -15,6 +15,29 @@ class IntroPage extends StatelessWidget {
           Column(
             children: [
               Spacer(),
+
+              //1.
+              Column(
+                children: [
+                  Image.asset(
+                    "assets/images/logo_deepdive.png",
+                    width: 160,
+                  ),
+                  Text(
+                    "다이버 쇼퍼들을 위한 공간",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+
+              Spacer(),
+              SizedBox(height: 40),
+
+              //2.
               Padding(
                 padding:
                     const EdgeInsets.symmetric(horizontal: 16, vertical: 40),
