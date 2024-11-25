fb5724825afce420728c9f08e6354f4c6b71002b
matthew <minwoo6263@gmail.com>
2024-11-25 11:06:46 +0900

    <chore>(main.dart): 샘플 코드 삭제

--- lib/main.dart
+++ lib/main.dart
@@ -9,26 +9,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark(),
-      home: const TakePictureScreen(),
-    );
-  }
-}
-
-class TakePictureScreen extends StatefulWidget {
-  const TakePictureScreen({super.key});
-
-  @override
-  TakePictureScreenState createState() => TakePictureScreenState();
-}
-
-class TakePictureScreenState extends State<TakePictureScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Select a picture')),
-      body: const Text('home'),
-    );
+    return MaterialApp();
   }
 }
