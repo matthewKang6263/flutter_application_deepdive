e5708184015c5d3bc1c27d8b16048afc34a4b9f1
akeetuitui <skj6164@naver.com>
2024-11-26 13:07:55 +0900

    <style>(quantityselector): 가격표시 텍스트 수정

--- lib/pages/detail/quantity_selector.dart
+++ lib/pages/detail/quantity_selector.dart
@@ -45,16 +45,29 @@ class QuantitySelector extends StatelessWidget {
                     ),
                   ),
                   SizedBox(height: 4),
-                  Text(
-                    "₩${totalPrice.toString()}",
-                    style: TextStyle(
-                      fontFamily: 'Pretendard',
-                      fontSize: 22,
-                      fontWeight: FontWeight.w600,
-                      letterSpacing: -0.005,
-                      height: 30 / 22,
-                      color: Color.fromRGBO(7, 112, 232, 1),
-                    ),
+                  Row(
+                    children: [
+                      Text(
+                        "₩${totalPrice.toString()}",
+                        style: TextStyle(
+                          fontFamily: 'Pretendard',
+                          fontSize: 22,
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: -0.005,
+                          height: 30 / 22,
+                          color: Color.fromRGBO(7, 112, 232, 1),
+                        ),
+                      ),
+                      SizedBox(height: 4),
+                      Text("원",
+                      style: TextStyle(
+                          fontFamily: 'Pretendard',
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                          height: 30 / 22,
+                          color: Color.fromRGBO(7, 112, 232, 1),
+                        ),)
+                    ],
                   ),
                 ],
               ),
