1c7ecf3a0b7fee016b9f451bea3d40c2512199d1
akeetuitui <skj6164@naver.com>
2024-11-26 11:57:08 +0900

    <style>(quantityselector): 상세페이지 수량조절 탭 UI 전체 수정(텍스트 스타일, 박스 크기 등)

--- lib/pages/detail/quantity_selector.dart
+++ lib/pages/detail/quantity_selector.dart
@@ -1,81 +1,90 @@
 import 'package:flutter/material.dart';
 
-// 수량 선택 UI 위젯
+/// 수량 조정 UI 위젯
 class QuantitySelector extends StatelessWidget {
+  final String itemName; // 상품 이름
   final int itemQuantity; // 현재 수량
   final int itemPrice; // 상품 단가
-  final ValueChanged<int> onQuantityChanged; // 수량 변경 시 호출되는 콜백
-  final VoidCallback onClose; // 닫기 버튼 클릭 시 호출되는 콜백
+  final Function(int) onQuantityChanged; // 수량 변경 콜백
+  final VoidCallback onClose; // 닫기 콜백
 
-  const QuantitySelector({
-    Key? key,
+  QuantitySelector({
+    required this.itemName,
     required this.itemQuantity,
     required this.itemPrice,
-    required this.onQuantityChanged, // 사용자가 수량 증가/감소 버튼 선택 시 콜백 호출, 새로운 수량 값 처리
-    required this.onClose, // 사용자가 닫기 버튼 누를 시 부모 위젯에서 UI를 닫는 동작 처리, 아코디언 버튼 닫힘
-  }) : super(key: key);
+    required this.onQuantityChanged,
+    required this.onClose,
+  });
 
   @override
   Widget build(BuildContext context) {
+    final totalPrice = itemQuantity * itemPrice; // 총 가격 계산
+
     return Container(
-      padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16), // 내부 여백
-      decoration: BoxDecoration(
-        color: Colors.white, // 배경색 흰색
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.2), // 그림자 효과
-            blurRadius: 5, // 그림자 흐림 정도
-            offset: Offset(0, -3), // 그림자의 방향과 거리
-          ),
-        ],
-      ),
-      
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
+      color: Colors.white,
       child: Column(
-        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // 총 금액 표시 및 닫기 버튼
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              IconButton(
-                icon: Icon(Icons.close, size: 24, color: Colors.grey), // 닫기 아이콘
-                onPressed: onClose,
+              // 상품 이름과 총 가격 표시
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    itemName,
+                    style: TextStyle(
+                      fontFamily: 'Pretendard',
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: -0.2,
+                      height: 24 / 18,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    "₩${totalPrice.toString()}",
+                    style: TextStyle(
+                      fontFamily: 'Pretendard',
+                      fontSize: 22,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: -0.005,
+                      height: 30 / 22,
+                      color: Color.fromRGBO(7, 112, 232, 1),
+                    ),
+                  ),
+                ],
               ),
-              Text(
-                '총 금액: ${(itemQuantity * itemPrice).toString()}원', // 총 금액 계산
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ),
-          
-          SizedBox(height: 12), // 간격
 
-          // 수량 조절 UI
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              // 감소 버튼
-              IconButton(
-                icon: Icon(Icons.remove_circle_outline,
-                    size: 32, color: Colors.blue),
-                onPressed: () => onQuantityChanged(itemQuantity - 1),
-              ),
-              // 현재 수량
-              Text(
-                itemQuantity.toString(),
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              // 증가 버튼
-              IconButton(
-                icon: Icon(Icons.add_circle_outline,
-                    size: 32, color: Colors.blue),
-                onPressed: () => onQuantityChanged(itemQuantity + 1),
+              // 수량 조절 버튼
+              Row(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.remove),
+                    onPressed: () {
+                      if (itemQuantity > 1) {
+                        onQuantityChanged(itemQuantity - 1); // 수량 감소 버튼
+                      }
+                    },
+                  ),
+                  Text(
+                    '$itemQuantity',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      fontFamily: 'Pretendard',
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.add),
+                    onPressed: () {
+                      onQuantityChanged(itemQuantity + 1); // 수량 증가 버튼
+                    },
+                  ),
+                ],
               ),
             ],
           ),
