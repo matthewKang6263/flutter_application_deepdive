faa93bd5f96b9e7eaaf05aeb3be7bb2612dc3940
akeetuitui <skj6164@naver.com>
2024-11-26 11:43:05 +0900

    <feat>(popupmessage): 상세페이지 장바구니 팝업 아이콘 및 텍스트 추가

--- lib/pages/detail/popup_message.dart
+++ lib/pages/detail/popup_message.dart
@@ -18,3 +18,47 @@ class PopupMessage extends StatelessWidget {
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+             // 1. 체크 이모티콘
+            Icon(
+              Icons.check_circle,
+              color: Colors.green, 
+              size: 16, 
+            ),
+            SizedBox(width: 6), // 이모티콘과 텍스트 간격
+
+            // 2. "장바구니에 상품이 담겼습니다" 텍스트
+            Expanded(
+              child: Text(
+                message, // 전달된 메시지
+                style: TextStyle(
+                  fontFamily: 'Pretendard', // 폰트
+                  fontSize: 15, // 텍스트 크기
+                  fontWeight: FontWeight.w700, // 텍스트 굵기
+                  color: Colors.white, // 텍스트 색상
+                ),
+              ),
+            ),
+            SizedBox(width: 5), // 텍스트와 "확인하기" 간격
+            
+            // 3. "확인하기" 링크
+            GestureDetector(
+              onTap: onConfirm, // 콜백 호출
+              child: Text(
+                "확인하기",
+                style: TextStyle(
+                  fontFamily: 'Pretendard',
+                  fontSize: 13,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.white,
+                  decoration: TextDecoration.underline, // 밑줄
+                  decorationStyle: TextDecorationStyle.solid,
+                  decorationColor: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
